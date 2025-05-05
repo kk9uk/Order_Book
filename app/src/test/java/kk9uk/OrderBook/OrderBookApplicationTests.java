eeec2cd1e11869857c6af7a26bc4d083a75a9a06
kk9uk <122882463+kk9uk@users.noreply.github.com>
2025-05-05 22:17:48 +0800

    [SPRING INIT]
    - Lombok
    - Spring Configuration Processor
    - Spring Web
    - Spring Data JPA
    - MariaDB Driver
    - Spring Boot Actuator

---
+++ app/src/test/java/kk9uk/OrderBook/OrderBookApplicationTests.java
@@ -0,0 +1,13 @@
+package kk9uk.OrderBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

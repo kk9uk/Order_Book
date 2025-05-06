d442dd6aa26ed9c6df78c92a55de618d56de5fe1
kk9uk <122882463+kk9uk@users.noreply.github.com>
2025-05-06 09:56:06 +0800

    [FEAT]: List orders

--- app/src/main/java/kk9uk/OrderBook/controller/Controller.java
+++ app/src/main/java/kk9uk/OrderBook/controller/Controller.java
@@ -2,27 +2,28 @@ package kk9uk.OrderBook.controller;
 
 import kk9uk.OrderBook.dto.PlaceRequestDto;
 import kk9uk.OrderBook.po.OrderPo;
+import kk9uk.OrderBook.repository.OrderRepository;
 import kk9uk.OrderBook.service.PlaceOrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class Controller {
 
     private final PlaceOrderService placeOrderService;
+    private final OrderRepository orderRepository;
 
     @PostMapping("/orders")
     public OrderPo placeOrder(@RequestBody PlaceRequestDto placeRequestDto) {
 
-        // Coordinates in request must be an array of exactly two strings
         if (placeRequestDto.origin().size() != 2 || placeRequestDto.destination().size() != 2) {
             throw new RuntimeException("Coordinates in request must be an array of exactly two strings");
         }
 
-        // The latitude and longitude value of coordinates must be correctly validated
         double startLat = Double.parseDouble(placeRequestDto.origin().get(0).trim()),
                startLong = Double.parseDouble(placeRequestDto.origin().get(1).trim()),
                endLat = Double.parseDouble(placeRequestDto.destination().get(0).trim()),
@@ -33,10 +34,22 @@ public class Controller {
                 (-180 > startLong || startLong > 180) ||
                 (-180 > endLong || endLong > 180)
         ) {
-            throw new RuntimeException("Invalid latitude and longitude value of coordinates");
+            throw new RuntimeException("Invalid latitude or longitude value in coordinates");
         }
 
         return placeOrderService.placeOrder(startLat, startLong, endLat, endLong);
+
+    }
+
+    @GetMapping("/orders")
+    public List<OrderPo> listOrders(@RequestParam int page, @RequestParam int limit) {
+
+        if (page < 1 || limit < 1) {
+            throw new RuntimeException("Invalid page or limit value");
+        }
+
+        // page is 0-based in Spring
+        return orderRepository.findAll(PageRequest.of(page - 1, limit)).getContent();
 
     }
 
--- app/src/main/java/kk9uk/OrderBook/po/OrderPo.java
+++ app/src/main/java/kk9uk/OrderBook/po/OrderPo.java
@@ -1,13 +1,17 @@
 package kk9uk.OrderBook.po;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Orders")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderPo {
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
   private long distance;
